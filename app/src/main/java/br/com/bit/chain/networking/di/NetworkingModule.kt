043a2a9406a80dfefbeb798f9f2d1556dc8d3409
Júlio Cotta <juliocbcotta@gmail.com>
2019-09-05 23:46:27 -0300

    added HttpLogginInterceptor to debug network requests

--- app/src/main/java/br/com/bit/chain/networking/di/NetworkingModule.kt
+++ app/src/main/java/br/com/bit/chain/networking/di/NetworkingModule.kt
@@ -2,6 +2,8 @@ package br.com.bit.chain.networking.di
 
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -10,12 +12,22 @@ import javax.inject.Singleton
 
 @Module
 class NetworkingModule {
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+            .build()
+    }
 
     @Provides
     @Singleton
-    fun provideRetrofit(@Named("BASE_URL") baseUrl: String): Retrofit {
+    fun provideRetrofit(client: OkHttpClient, @Named("BASE_URL") baseUrl: String): Retrofit {
 
         return Retrofit.Builder()
+            .client(client)
             .baseUrl(baseUrl)
             .addCallAdapterFactory(RxJava2CallAdapterFactory.createAsync())
             .addConverterFactory(GsonConverterFactory.create())
