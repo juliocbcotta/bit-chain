3241096d244b5810ff6947f9e8f7efc50d55c613
Júlio Cotta <juliocbcotta@gmail.com>
2019-09-07 01:03:36 -0300

    started migrating dependencies to external common file.

---
+++ buildSrc/src/main/java/dependencies.kt
@@ -0,0 +1,12 @@
+object Dependencies {
+
+    const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.50"
+
+    const val dagger = "com.google.dagger:dagger:2.24"
+    const val dagger_compiler = "com.google.dagger:dagger-compiler:2.24"
+
+    const val dagger_android = "com.google.dagger:dagger-android:2.24"
+    const val dagger_android_compiler = "com.google.dagger:dagger-android-processor:2.24"
+
+
+}
