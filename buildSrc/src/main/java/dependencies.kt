68446657c91903ef54a22856e58cdb2cd5caf9fc
Júlio Cotta <juliocbcotta@gmail.com>
2019-09-07 01:28:40 -0300

    migrated more dependencies to external common file.

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -2,11 +2,23 @@ object Dependencies {
 
     const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.50"
 
+    const val androidx_core = "androidx.core:core-ktx:1.1.0"
+    const val androidx_appcompat = "androidx.appcompat:appcompat:1.1.0"
+    const val androidx_preferences = "androidx.preference:preference:1.1.0"
+    const val androidx_lifecycle_extensions = "androidx.lifecycle:lifecycle-extensions:2.0.0"
+
+    const val androidx_constraint_layout = "androidx.constraintlayout:constraintlayout:1.1.3"
+    const val androidx_cardview = "androidx.cardview:cardview:1.0.0"
+
     const val dagger = "com.google.dagger:dagger:2.24"
     const val dagger_compiler = "com.google.dagger:dagger-compiler:2.24"
 
     const val dagger_android = "com.google.dagger:dagger-android:2.24"
     const val dagger_android_compiler = "com.google.dagger:dagger-android-processor:2.24"
 
+    const val rx_android = "io.reactivex.rxjava2:rxandroid:2.1.1"
+    const val rx_java = "io.reactivex.rxjava2:rxjava:2.2.12"
+
+    const val mp_android_chart = "com.github.PhilJay:MPAndroidChart:v3.1.0"
 
 }
