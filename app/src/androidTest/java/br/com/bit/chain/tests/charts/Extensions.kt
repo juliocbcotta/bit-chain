e9d9b7783e3ccf13aab8f1cea8b3ebb2d78f7f6a
Júlio Cotta <juliocbcotta@gmail.com>
2019-09-08 17:12:38 -0300

    fix json definition

--- app/src/androidTest/java/br/com/bit/chain/tests/charts/Extensions.kt
+++ app/src/androidTest/java/br/com/bit/chain/tests/charts/Extensions.kt
@@ -2,23 +2,41 @@ package br.com.bit.chain.tests.charts
 
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+val calendar = Calendar.getInstance().apply {
+    set(Calendar.YEAR, 2019)
+    set(Calendar.MONTH, 8)
+    set(Calendar.DAY_OF_MONTH, 8)
+    set(Calendar.HOUR, 0)
+    set(Calendar.MINUTE, 0)
+    set(Calendar.SECOND, 0)
+    set(Calendar.MILLISECOND, 0)
+}
+
+val x = (calendar.time.time / 1000).toFloat()
+val x1 = ((calendar.time.time + TimeUnit.DAYS.toSeconds(1) )/ 1000).toFloat()
+val x2 = ((calendar.time.time + TimeUnit.DAYS.toSeconds(2) )/ 1000).toFloat()
+
 
 val chartDataResponseJson = """
                     {
                         "name": "name",
                         "description" : "description",
                         "unit" : "unit",
+                        "period": "day",
                         "values" :[
                                 {
-                                 "x" : 1.0,
+                                 "x" : $x,
                                  "y" : 2.0
                                 },
                                  {
-                                 "x" : 2.0,
+                                 "x" : $x1,
                                  "y" : 4.0
                                 },
                                  {
-                                 "x" : 3.0,
+                                 "x" : $x2,
                                  "y" : 6.0
                                 },
                                  {
